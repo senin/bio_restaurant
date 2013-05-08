5d005763e796b3f8295ab8207039844c8476c73d
Dmitry Sen <dmitryssen@gmail.com>
2013-05-08 10:17:28 +0300

    Added ORM entities. Added maven jetty plugin  support

---
+++ src/main/java/ua/bionic/sen/db/DishCategory.java
@@ -0,0 +1,14 @@
+package ua.bionic.sen.db;
+
+import javax.persistence.Entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dmitry
+ * Date: 08.05.13
+ * Time: 10:14
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class DishCategory {
+}
