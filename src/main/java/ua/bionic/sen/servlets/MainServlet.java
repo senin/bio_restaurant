f5f3037fe764647545a16ff667aaee9702247a3f
Dmitry Sen <dmitryssen@gmail.com>
2013-05-10 11:54:54 +0300

    Updated MainServlet

--- src/main/java/ua/bionic/sen/servlets/MainServlet.java
+++ src/main/java/ua/bionic/sen/servlets/MainServlet.java
@@ -1,5 +1,10 @@
 package ua.bionic.sen.servlets;
 
+import ua.bionic.sen.db.Dish;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,16 +13,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Created with IntelliJ IDEA.
- * User: dmitry
- * Date: 08.05.13
- * Time: 1:08
- * To change this template use File | Settings | File Templates.
+ *
  */
 @WebServlet(urlPatterns = "/main")
 public class MainServlet extends HttpServlet {
+
+  private static EntityManager em;
+
+  @Override
+  public void init() throws ServletException {
+    super.init();
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("bionic_restaurant");
+    Dish dish = new Dish();
+    dish.setTitle("dish1");
+    em = emf.createEntityManager();
+    em.persist(dish);
+  }
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    resp.getWriter().println("hello");
+    resp.getWriter().println(em.find(Dish.class,1));
   }
 }
